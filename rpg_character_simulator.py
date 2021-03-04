dd18df69c6381449c4dc79e79e2c6ddf05e2b80b
Caleb Evans <caleb39411@gmail.com>
2021-03-04 13:32:23 +0000

    Improve formatting and fix incorrect function calls

--- rpg_character_simulator.py
+++ rpg_character_simulator.py
@@ -10,9 +10,9 @@ from tabulate import tabulate
 
 
 attr_dict = {
-    '1': ['power', 'Power', ]
-    '2': ['sp_power', 'Special Power',]
-    '3': ['speed', 'Speed',]
+    1: ['power', 'Power'],
+    2: ['sp_power', 'Special Power'],
+    3: ['speed', 'Speed'],
 }
 
 headers = [
@@ -32,7 +32,7 @@ def prompt(query):
 
 
 def name_gen():
-    vowels = ['a', 'e', 'i', 'o', 'u', ]
+    vowels = ['a', 'e', 'i', 'o', 'u']
     consonants = [
         'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
         'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z',
@@ -44,17 +44,17 @@ def name_gen():
         '-' +
         random.choice(consonants) +
         random.choice(vowels) +
-        random.choice([random.choice(consonants), str()]) +
+        random.choice([random.choice(consonants), '']) +
         '-' +
         random.choice(consonants) +
         random.choice(vowels) +
-        random.choice([random.choice(consonants), str()])
+        random.choice([random.choice(consonants), ''])
     )
 
 
 def gen_table():
     global table
-    table = list()
+    table = []
     for _ in range(10):
         table.append([
             _ + 1,
@@ -77,7 +77,7 @@ def char_replace():
         return char_replace()
     if 1 <= replace_no <= 10:
         globals()['char{0}'.format(replace_no - 1)] = random.choice(classes)()
-        print('Character', str(replace_no), 'replaced.', )
+        print('Character', str(replace_no), 'replaced.')
         time.sleep(1.5)
         gen_table()
         return menu()
@@ -91,30 +91,35 @@ def char_edit():
             'Enter the number of the character to be edited [1-10]: '
         ))
     except ValueError:
-        print('\n\x1b[31m' + 'Please enter an number [1-10]:' + '\x1b[0m')
-        return char_replace()
+        print('\n\x1b[31m' + 'Please enter a number [1-10]:' + '\x1b[0m')
+        return char_edit()
     if 1 <= edit_no <= 10:
         print(
-            "You are editing {0}".format(
+            'You are editing {0}'.format(
                 globals()['char{0}'.format(edit_no - 1)].name
             )
         )
         for entry in attr_dict:
-            print('{0}. {1}'.format(entry, attr_dict[entry][1], ))
-        attr_select = input('Which attribute is to be changed? [1-3]: ')
-        return attr_edit(attr_select, edit_no)
+            print('{0}. {1}'.format(entry, attr_dict[entry][1]))
+        try:
+            attr_select = int(input('Which attribute is to be changed? [1-3]: '))
+        except ValueError:
+            print('\n\x1b[31m' + 'Please enter a number [1-10]:' + '\x1b[0m')
+            return char_edit()
+        if 1 <= attr_select <= 3:
+            return attr_edit(attr_select, edit_no)
     print('\n\x1b[31m' + 'Please enter a valid number.' + '\x1b[0m')
-    return char_replace()
+    return char_edit()
 
 
-def attr_edit(attr_no, char_no, ):
+def attr_edit(attr_no, char_no):
     try:
         attr_val = int(input(
             'Enter the new value of the attribute [1-100]: '
         ))
     except ValueError:
         print('\n\x1b[31m' + 'Please enter a valid number.' + '\x1b[0m')
-        return attr_edit(attr_no, char_no, )
+        return attr_edit(attr_no, char_no)
     if 1 <= attr_val <= 100:
         setattr(
             globals()['char{0}'.format(char_no - 1)],
@@ -124,7 +129,7 @@ def attr_edit(attr_no, char_no, ):
         gen_table()
         return menu()
     print('\n\x1b[31m' + 'Please enter a valid number.' + '\x1b[0m')
-    return attr_edit(attr_no, char_no, )
+    return attr_edit(attr_no, char_no)
 
 
 def save_data():
@@ -172,18 +177,18 @@ def exit_check():
 
 
 menu_dict = {
-    1: [char_replace, 'Replace character', ],
-    2: [char_edit, 'Edit character attribute', ],
-    3: [save_data, 'Save', ],
-    4: [load_data, 'Load', ],
-    5: [exit_check, 'Exit', ],
+    1: [char_replace, 'Replace character'],
+    2: [char_edit, 'Edit character attribute'],
+    3: [save_data, 'Save'],
+    4: [load_data, 'Load'],
+    5: [exit_check, 'Exit'],
 }
 
 def menu():
-    print(tabulate(table, headers, ))
+    print(tabulate(table, headers))
     input('\nPress ENTER to continue to menu:\n')
     for entry in menu_dict:
-        print('{0}. {1}'.format(entry, menu_dict[entry][1], ))
+        print('{0}. {1}'.format(entry, menu_dict[entry][1]))
     selection = int(input('Please select an option [1-5]: '))
     if 1 <= selection <= 5:
         menu_dict[selection][0]()
@@ -197,56 +202,56 @@ def menu():
 
 
 class Barbarian:
-    class_name = str('Barbarian')
-    power = int(70)
-    sp_power = int(20)
-    speed = int(50)
+    class_name = 'Barbarian'
+    power = 70
+    sp_power = 20
+    speed = 50
 
     def __init__(self):
         self.name = name_gen()
 
 
 class Elf:
-    class_name = str('Elf')
-    power = int(30)
-    sp_power = int(60)
-    speed = int(10)
+    class_name = 'Elf'
+    power = 30
+    sp_power = 60
+    speed = 10
 
     def __init__(self):
         self.name = name_gen()
 
 
 class Wizard:
-    class_name = str('Wizard')
-    power = int(50)
-    sp_power = int(70)
-    speed = int(30)
+    class_name = 'Wizard'
+    power = 50
+    sp_power = 70
+    speed = 30
 
     def __init__(self):
         self.name = name_gen()
 
 
 class Dragon:
-    class_name = str('Dragon')
-    power = int(90)
-    sp_power = int(40)
-    speed = int(50)
+    class_name = 'Dragon'
+    power = 90
+    sp_power = 40
+    speed = 50
 
     def __init__(self):
         self.name = name_gen()
 
 
 class Knight:
-    class_name = str('Knight')
-    power = int(60)
-    sp_power = int(10)
-    speed = int(60)
+    class_name = 'Knight'
+    power = 60
+    sp_power = 10
+    speed = 60
 
     def __init__(self):
         self.name = name_gen()
 
 
-classes = [Barbarian, Elf, Wizard, Dragon, Knight, ]
+classes = [Barbarian, Elf, Wizard, Dragon, Knight]
 
 for _ in range(10):
     globals()['char{0}'.format(_)] = random.choice(classes)()
